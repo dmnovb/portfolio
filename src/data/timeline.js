7bc54548d2fd5e38a5e267458a7a39a14a48f481
bobi <boqn.damqnov03@gmail.com>
2023-01-26 16:12:47 +0200

    feat: created timeline

---
+++ src/data/timeline.js
@@ -0,0 +1,41 @@
+export default [
+    {
+        year: 2017,
+        background: `Started coding for the first time in school.
+        Initially I was interested in cybersecurity, however as we kept studying algorithms, 
+        data structures and how the computer works I was much more interested in becoming a software developer grabbed me much more,
+        ever since then I have not stopped coding.`,
+        skills: ['Basic computer and internet navigation skills']
+    },
+    {
+        year: 2018,
+        background: `I made my first app. It was a student database made in Python. For the UI I used a library called Tkinter. 
+        The database was SQL. 
+        As the app progressed I got even more interested in a career in software development 
+        because I was excited that something I made worked.`,
+        skills: ['Python']
+    },
+    {
+        year: 2019,
+        background: `I made some simple games with python using the library Pygame. 
+        I also delved into making websites 
+        using Django and was able to create one for the first time.`,
+        skills: ['Python', 'Pygame', 'Flask', 'Django']
+    },
+    {
+        year: 2020,
+        background: `I created my first HTML landing page. 
+        From that moment I was interested in developing myself 
+        as a front-end engineer.`,
+        skills: ['HTML', 'CSS']
+    },
+    {
+        year: 2022,
+        background: `I have now created 10+ projects of my own. 
+        I'm confident in my 
+        abilities and am 
+        looking forward to finding my first job as a 
+        front-end engineer.`,
+        skills: ['Javascript', 'HTML', 'CSS', 'Express.js', 'MongoDB', 'React.js', 'Tailwind', 'Problem Solving Skills']
+    }
+]
